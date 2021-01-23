055c9b109b8f7629d8ab14d0a1deb388ff200369
Kade Boltjes <kboltjes@purdue.edu>
2021-01-23 15:48:01 -0500

    Added Google Notification

--- Chrome Extension/js/netflix.js
+++ Chrome Extension/js/netflix.js
@@ -1,94 +1,28 @@
 // this holds the last url that had "https://www.netflix.com/watch/" in its name.
 // this way it doesn't keep updating the page with the same show/movie repeatedly
 let lastNetflixUrl = "";
-
-
+let tabID = 0;
 /*
-let url = "https://www.netflix.com/title/80057281";
+function newPopup(url) {
+	popupWindow = window.open(url,'popUpWindow','height=300,width=400,left=10,top=10,resizable=false,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}*/
 
-const myInit = {
-    method: 'GET',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    credentials: 'omit'
-};
-  
-fetch(url, myInit)
-.then(response => response.text())
-.then(data => {
+/* Respond to the user's clicking one of the buttons */
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+        if (btnIdx === 0) {
+            console.log("YES");
+        } else if (btnIdx === 1) {
+            console.log("NO");
+        }
     
-        const regexForTitle = /"name":".*?","description":"/;
-        const regexForImage = /","image":".*?","dateCreated":"/;
-        const regexForActors = /,"actors":\[{"@type":".*?}\],"creator":/;
-        const regexForCreator = /,"creator":\[{"@type":"Person","name":".*?"}],"director":/;
-        const regexForDirector = /,"director":\[{"@type":"Person","name":".*?"}],"awards":/;
+});
 
-        let title = "";
-        try {
-            title = data.match(regexForTitle)[0];
-            title = title.replace("\"name\":\"", "").replace("\",\"description\":\"", "");
-        } catch (error) {}
 
-        
-        let image = "";
-        try {
-            image = data.match(regexForImage)[0];
-            image = image.replace("\",\"image\":\"", "").replace("\",\"dateCreated\":\"", "");
-        } catch (error) {}
-
-        let actors = "";
-        let actorList = [];
-        try {
-            actors = data.match(regexForActors)[0];
-            actors = actors.replace(",\"actors\":[", "").replace("\"}],\"creator\":", "")
-                .replaceAll("{\"@type\":\"Person\",\"name\":\"", "").replaceAll("\"}", "").replaceAll("{", "");
-            actorList = actors.split(",");
-        } catch (error) {}
-
-        let creator = "";
-        try {
-            creator = data.match(regexForCreator)[0];
-            creator = creator.replace(",\"creator\":\[{\"@type\":\"Person\",\"name\":\"", "").replace("\"}],\"director\":", "");
-        } catch (error) {}
-
-        let director = "";
-        try {
-            director = data.match(regexForDirector)[0];
-            director = director.replace(",\"director\":\[{\"@type\":\"Person\",\"name\":\"", "").replace("\"}],\"awards\":", "");
-        } catch (error) {}
-        
-
-        console.log(title);
-        console.log(image);
-        console.log(actors);
-        console.log(actorList);
-        console.log(creator);
-        console.log(director);
-    });
-
-*/
-/*
-let xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      const regex = /"name":".*?","description":"/;
-      let found = xhr.responseText.match(regex)[0];
-      found = found.replace("\"name\":\"", "").replace("\",\"description\":\"", "");
-      console.log(found);
-   }};
-
-xhr.send();
-*/
 function scanTabs() {
     chrome.windows.getAll({populate: true}, function(windows) {
         chrome.tabs.getSelected(null,function(tab) {
             let tablink2 = tab.url;
-            chrome.extension.getBackgroundPage().console.log(tablink2);
             if (tablink2.includes("https://www.netflix.com/watch/") && lastNetflixUrl != tablink2) {
-                // now we know they are watching a netflix show.
                 lastNetflixUrl = tablink2;
                 const regex = /https:\/\/www.netflix.com\/watch\/[0-9]*/;
                 let found = lastNetflixUrl.match(regex)[0];
@@ -105,8 +39,6 @@ function scanTabs() {
                     cache: 'no-cache',
                     credentials: 'omit'
                 };
-
-                console.log(lastNetflixUrl);
 
                 fetch(lastNetflixUrl, myInit)
                 .then(response => response.text())
@@ -158,7 +90,21 @@ function scanTabs() {
                             'Poster': image
                         };
                         addShow(finalData);
-                        });
+                            
+                        console.log("sending");
+                        chrome.notifications.create('', {
+                            title: 'SpoilerFilter',
+                            message: 'Do you want to add \"' + title + '\" to SpoilerFilter?',
+                            iconUrl: '/icon.png',
+                            type: 'basic',
+                            buttons: [{
+                                title: "Yes",
+                            }, {
+                                title: "No",
+                            }]
+                            });
+                });
+                        
             }
         });
     });
