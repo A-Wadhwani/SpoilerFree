49602f38c32b3345d67cbd02953f1c911e49e85b
Kade Boltjes <kboltjes@purdue.edu>
2021-01-22 22:34:31 -0500

    Added scanning for netflix watching

---
+++ Chrome Extension/js/netflix.js
@@ -0,0 +1,26 @@
+// this holds the last url that had "https://www.netflix.com/watch/" in its name.
+// this way it doesn't keep updating the page with the same show/movie repeatedly
+let lastNetflixUrl = "";
+
+function scanTabs() {
+    chrome.windows.getAll({populate: true}, function(windows) {
+        chrome.tabs.getSelected(null,function(tab) {
+            let tablink2 = tab.url;
+            chrome.extension.getBackgroundPage().console.log(tablink2);
+            if (tablink2.includes("https://www.netflix.com/watch/") && lastNetflixUrl != tablink2) {
+                // now we know they are watching a netflix show.
+                lastNetflixUrl = tablink2;
+                const regex = /https:\/\/www.netflix.com\/watch\/[0-9]*/;
+                let found = lastNetflixUrl.match(regex)[0];
+                let showId = found.substring(30, found.length);
+                
+                // print debugging stuff
+                chrome.extension.getBackgroundPage().console.log("Watching Netflix");
+                chrome.extension.getBackgroundPage().console.log(showId);
+            }
+        });
+    });
+}
+
+  chrome.tabs.onUpdated.addListener(scanTabs);
+  chrome.tabs.onCreated.addListener(scanTabs);
